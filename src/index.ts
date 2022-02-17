f4e57f4fca2f82b91934a15684c5da34d406ceb2
Nam Nguyen <namnguyn3019@gmail.com>
2022-02-17 12:08:50 -0600

    Set up express app and connect to mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-// Write your node app here
+import express from "express";
+import connectDB from "./dbconfig";
+const app = express();
+const port: number = 5001;
+
+connectDB();
+app.get("/", (req, res) => {
+  res.send("Hello from the other side");
+});
+app.listen(port, () => {
+  console.log(`App is running on port ${port} `);
+});
